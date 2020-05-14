6c09a6fefb2333138bf9c3ed346ee1dfa234a2ec
Drago <drago.jelavic2497@gmail.com>
2020-05-14 19:30:10 +0200

    v2.4

--- src/core/ShowImage.js
+++ src/core/ShowImage.js
@@ -2,11 +2,11 @@ import React from "react";
 import { API } from "../config";
 
 const ShowImage = ({ item, url }) => (
-  <div className="product-img">
+  <div className="product-img text-center">
     <img
       src={`${API}/${url}/photo/${item._id}`}
       alt={item.name}
-      className="mb-3 text-center"
+      className="mb-3"
       style={{ maxHeight: "100%", maxWidth: "100%" }}
     />
   </div>
