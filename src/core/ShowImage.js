9fca993dd31611ca8fe2f6156ef7b2cb3405f14b
Drago <drago.jelavic2497@gmail.com>
2020-05-14 19:26:11 +0200

    v2.4

--- src/core/ShowImage.js
+++ src/core/ShowImage.js
@@ -6,7 +6,7 @@ const ShowImage = ({ item, url }) => (
     <img
       src={`${API}/${url}/photo/${item._id}`}
       alt={item.name}
-      className="mb-3"
+      className="mb-3 text-center"
       style={{ maxHeight: "100%", maxWidth: "100%" }}
     />
   </div>
